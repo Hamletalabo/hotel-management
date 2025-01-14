20128eb377565eff80dd0f66b2b162eb7b5f0a50
Hamletalabo <hamletalabo1@gmail.com>
2025-01-14 21:44:51 +0100

    done setting up the application security

--- src/main/java/com/hamlet/HamletHotel/exception/GlobalException.java
+++ src/main/java/com/hamlet/HamletHotel/exception/GlobalException.java
@@ -5,15 +5,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+@ControllerAdvice
+public class GlobalException {
 
-
-public class GlobalException extends RuntimeException{
-    public GlobalException (String message){
-        super(message);
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<Object> handleNotFoundException(NotFoundException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
     }
-
-//    @ExceptionHandler(UserNotFoundException.class)
-//    public ResponseEntity<String> handleUserNotFoundException(UserNotFoundException ex) {
-//        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
-//    }
 }
